fccd0cb35573cba0c0ede7b897afb56aa9d90408
Stephen A. Wilson <Stephen-356@hotmail.com>
2018-04-29 16:31:16 -0400

    add script for getting the DB set up
    creates dev and test databases and a user to access them

---
+++ server/db/init.sql
@@ -0,0 +1,10 @@
+-- Create dev and test databases
+CREATE DATABASE taskka_dev;
+CREATE DATABASE taskka_test;
+
+-- Create the user that will access the databases. Be sure to change the password!
+CREATE USER taskka WITH PASSWORD 'taskka_login';
+
+-- Grant taskka access to the databases
+GRANT ALL ON DATABASE taskka_dev TO taskka;
+GRANT ALL ON DATABASE taskka_test TO taskka;
