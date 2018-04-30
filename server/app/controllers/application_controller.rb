b3000df7f62c8e8759c62fa9cf1f72ac6e0b75b8
Stephen A. Wilson <Stephen-356@hotmail.com>
2018-04-30 13:26:20 -0400

    create authorize action filter

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -1,2 +1,29 @@
 class ApplicationController < ActionController::API
+  class NotAuthorized < StandardError; end
+
+  rescue_from ApplicationController::NotAuthorized, :with => :not_authorized
+
+  def authorize
+    if !request.headers["Authorization"]
+      raise ApplicationController::NotAuthorized
+    end
+
+    type, access_token = request.headers["Authorization"].split(' ')
+    if type != 'Bearer'
+      raise ApplicationController::NotAuthorized
+    end
+
+    user = User.find_by_access_token(access_token)
+    if !user
+      raise ApplicationController::NotAuthorized
+    end
+
+    request.env["current_user"] = user
+  end
+
+  private
+
+  def not_authorized
+    render json: { error: "Not Authorized" }, status: 401
+  end
 end
---
+++ server/spec/controllers/application_controller_spec.rb
@@ -0,0 +1,58 @@
+describe ApplicationController do
+  controller do
+    before_action :authorize
+    def index; end
+  end
+
+  before do
+    routes.draw do
+      get 'index' => 'anonymous#index'
+    end
+  end
+
+  describe '#authorize' do
+    context "when there is no Authorization header" do
+      before { request.headers["Authorization"] = nil }
+
+      it "responds with a not authorized response" do
+        get :index
+
+        expect(response.status).to eq(401)
+        expect(JSON.parse(response.body)["error"]).to eq("Not Authorized")
+      end
+    end
+
+    context "when the Authorization is not Bearer" do
+      before { request.headers["Authorization"] = "Basic ZXhhbXBsZTpleGFtcGxl" }
+
+      it "responds with a not authorized response" do
+        get :index
+
+        expect(response.status).to eq(401)
+        expect(JSON.parse(response.body)["error"]).to eq("Not Authorized")
+      end
+    end
+
+    context "when the access_token doesn't match a user" do
+      before { request.headers["Authorization"] = "Bearer Example-Token" }
+
+      it "responds with a not authorized response" do
+        get :index
+
+        expect(response.status).to eq(401)
+        expect(JSON.parse(response.body)["error"]).to eq("Not Authorized")
+      end
+    end
+
+    context "when the access_token matches a user" do
+      let (:user) { User.create() }
+      
+      before { request.headers["Authorization"] = "Bearer #{user.access_token}" }
+
+      it "lets the action run" do
+        expect(controller).to receive(:index)
+        get :index
+      end
+    end
+  end
+end
