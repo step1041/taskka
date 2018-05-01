9deff27e1598241e51bf6ec73126ead3d34e3d63
Stephen A. Wilson <Stephen-356@hotmail.com>
2018-05-01 18:24:20 -0400

    update AppLayout

--- client/src/layouts/app.layout.js
+++ client/src/layouts/app.layout.js
@@ -1,25 +1,38 @@
-import React from "react";
+  import React from "react";
+import {connect} from 'react-redux';
+import {Route, Redirect} from 'react-router'
 
-import {Route, Switch} from 'react-router-dom';
+import ConnectedSwitch from '../lib/connected-switch';
 
 import VerifyPage from '../auth/verify.page';
 import LoginPage from '../auth/login.page';
 import TaskListPage from '../tasks/task-list.page';
 import NewUserPage from '../user/new-user.page';
 
-const AppLayout = () => (
+const mapStateToProps = (state) => ({
+  userLoggedIn: state.user !== null,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+});
+
+const AppLayout = ({userLoggedIn}) => (
   <div>
-    <Switch>
-      <Route exact path={'/'} component={LoginPage} />
+    AppLayout
+    <ConnectedSwitch>
+      <Route path={'/login'} component={LoginPage} />
       <Route path={'/auth/:provider/callback'} component={VerifyPage} />
       <Route path={'/user/new'} component={NewUserPage} />
       <Route path={'/tasks'} component={TaskListPage} />
       <Route>
-        <div>Default</div>
+        { userLoggedIn ? <Redirect to={'/tasks'} /> : <Redirect to={'/login'} /> }
       </Route>
-    </Switch>
+    </ConnectedSwitch>
   </div>
-);
 
-export default AppLayout;
+);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(AppLayout);
 
