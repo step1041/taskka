08f577fdbde6e5e2ee6a9667e2fd216e3070d6a6
Stephen A. Wilson <Stephen-356@hotmail.com>
2018-05-02 12:27:27 -0400

    retreive username when loading task list

--- client/src/tasks/task-list.page.js
+++ client/src/tasks/task-list.page.js
@@ -1,10 +1,29 @@
 import React, {Component} from 'react';
 
+import TaskkaApiClient from '../taskka-api-client';
+import errorHandler from '../lib/error-handler';
+
 class TaskListPage extends Component {
+  constructor() {
+    super();
+
+    this.state = { username: <em>&lt;loading&gt;</em> }
+  }
+
+  componentWillMount() {
+    TaskkaApiClient
+      .get('/user/current_user')
+      .then((data) => {
+        let username = data.user.username;
+        this.setState({ username });
+      })
+      .catch(errorHandler);
+  }
+
   render() {
     return (
       <div>
-        List of your tasks...
+        Hello {this.state.username}! Here are a list of your tasks...
       </div>
     )
   }
