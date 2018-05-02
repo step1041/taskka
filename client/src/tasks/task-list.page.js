8145f76887b793d037e47ee4b25e39e9176c3de6
Stephen A. Wilson <Stephen-356@hotmail.com>
2018-05-02 15:56:25 -0400

    display the list of tasks

--- client/src/tasks/task-list.page.js
+++ client/src/tasks/task-list.page.js
@@ -1,11 +1,22 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import TaskkaApiClient from '../lib/taskka-api-client';
+import {setTasks} from '../actions/task.actions';
+import errorHandler from '../lib/error-handler';
 
 const mapStateToProps = (state) => ({
   user: state.user,
+  tasks: state.tasks,
 });
 
 class TaskListPage extends Component {
+  componentWillMount() {
+    TaskkaApiClient
+      .get('/tasks')
+      .then(({tasks}) => this.props.dispatch(setTasks(tasks)))
+      .catch(errorHandler)
+  }
+
   render() {
     if (!this.props.user) {
       return (<div>Loading...</div>)
@@ -13,6 +24,11 @@ class TaskListPage extends Component {
     return (
       <div>
         Hello {this.props.user.username}! Here are a list of your tasks...
+        <ul>
+          {this.props.tasks.map((task) => (
+            <li key={task.id}>{task.name} ({task.state})</li>
+          ))}
+        </ul>
       </div>
     )
   }
