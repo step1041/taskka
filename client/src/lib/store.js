687431e770d4d0412c8d67386b71bb8cd3e23f25
Stephen A. Wilson <Stephen-356@hotmail.com>
2018-05-02 17:44:50 -0400

    apply thunk middleware

--- client/src/lib/store.js
+++ client/src/lib/store.js
@@ -1,15 +1,19 @@
 import {applyMiddleware, combineReducers, createStore} from 'redux';
 import {composeWithDevTools} from 'redux-devtools-extension';
-import reducers from '../reducers';
 import {routerMiddleware} from 'react-router-redux';
+import thunk from 'redux-thunk';
+
 import createHistory from 'history/createBrowserHistory';
+
+import reducers from '../reducers';
 
 const history = createHistory();
 
 const store = createStore(
   combineReducers(reducers),
   composeWithDevTools(
-    applyMiddleware(routerMiddleware(history))
+    applyMiddleware(routerMiddleware(history)),
+    applyMiddleware(thunk),
   )
 );
 
