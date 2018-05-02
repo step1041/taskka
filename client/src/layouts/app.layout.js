fdd01b307f98ff1d29b25c01bed485e71a2b8cd1
Stephen A. Wilson <Stephen-356@hotmail.com>
2018-05-02 12:29:03 -0400

    update AppLayout

--- client/src/layouts/app.layout.js
+++ client/src/layouts/app.layout.js
@@ -1,4 +1,4 @@
-  import React from "react";
+import React, { Component } from "react";
 import {connect} from 'react-redux';
 import {Route, Redirect} from 'react-router'
 
@@ -8,31 +8,32 @@ import VerifyPage from '../auth/verify.page';
 import LoginPage from '../auth/login.page';
 import TaskListPage from '../tasks/task-list.page';
 import NewUserPage from '../user/new-user.page';
+import TaskkaApiClient from '../taskka-api-client';
 
 const mapStateToProps = (state) => ({
-  userLoggedIn: state.user !== null,
+  userLoggedIn: TaskkaApiClient.getAccessToken() !== null,
 });
 
-const mapDispatchToProps = (dispatch) => ({
-});
+class AppLayout extends Component {
+  render () {
+    return (
+      <div>
+        AppLayout
+        <ConnectedSwitch>
+          <Route path={'/login'} component={LoginPage} />
+          <Route path={'/auth/:provider/callback'} component={VerifyPage} />
+          <Route path={'/user/new'} component={NewUserPage} />
+          <Route path={'/tasks'} component={TaskListPage} />
+          <Route>
+            { this.props.userLoggedIn ? <Redirect to={'/tasks'} /> : <Redirect to={'/login'} /> }
+          </Route>
+        </ConnectedSwitch>
+      </div>
+    );
+  }
+}
 
-const AppLayout = ({userLoggedIn}) => (
-  <div>
-    AppLayout
-    <ConnectedSwitch>
-      <Route path={'/login'} component={LoginPage} />
-      <Route path={'/auth/:provider/callback'} component={VerifyPage} />
-      <Route path={'/user/new'} component={NewUserPage} />
-      <Route path={'/tasks'} component={TaskListPage} />
-      <Route>
-        { userLoggedIn ? <Redirect to={'/tasks'} /> : <Redirect to={'/login'} /> }
-      </Route>
-    </ConnectedSwitch>
-  </div>
-
-);
 export default connect(
   mapStateToProps,
-  mapDispatchToProps,
 )(AppLayout);
 
