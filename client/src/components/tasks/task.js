4832c233e7e8e874c2668d5ebc8b91d02b9f58e5
Stephen A. Wilson <Stephen-356@hotmail.com>
2018-05-03 14:37:29 -0400

    add editing of tasks

--- client/src/components/tasks/task.js
+++ client/src/components/tasks/task.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 
 import {deleteTask, updateTask} from '../../actions/task.actions';
+import debounce from 'debounce';
 
 const mapStateToProps = (state) => ({
   tasks: state.tasks,
@@ -12,47 +13,97 @@ class Task extends Component {
     super();
 
     this.initState = {
+      errors: {},
       submitting: false,
-      checked: null,
+      task: {},
     };
 
     this.state = this.initState;
 
-    this.onTaskStateChange = this.onTaskStateChange.bind(this);
     this.onTaskDelete = this.onTaskDelete.bind(this);
+    this.onTaskStateChange = this.onTaskStateChange.bind(this);
+    this.onTaskNameChange = this.onTaskNameChange.bind(this);
+  }
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+    return {
+      task: nextProps.task,
+    };
   }
 
   render() {
-    let {task} = this.props;
-    let checked = null;
-
-    if (this.state.checked === null) {
-      checked = task.state === 'complete';
-    }
-    else {
-      checked = this.state.checked;
-    }
-
     return (
-      <li key={task.id}>
-        <label>
-          <input type={'checkbox'} checked={checked} onChange={this.onTaskStateChange}
-                 disabled={this.state.submitting} />{task.name}
-        </label>
-        <button onClick={this.onTaskDelete} disabled={this.state.submitting}>Delete</button>
+      <li key={this.state.task.id}>
+        <input
+          type={'checkbox'}
+          checked={this.state.task.state === 'complete'}
+          onChange={this.onTaskStateChange}
+          disabled={this.state.submitting}
+        />
+        <input
+          type={'text'}
+          value={this.state.task.name}
+          onChange={this.onTaskNameChange}
+          disabled={this.state.submitting}
+        />
+        <button
+          onClick={this.onTaskDelete}
+          disabled={this.state.submitting}
+        >
+          Delete
+        </button>
+        {this.state.errors && this.state.errors.name
+          ? <small>{this.state.errors.name}</small>
+          : null
+        }
       </li>
     );
+  }
+
+  updateTaskDebounced = debounce(() => {
+    this.props
+      .dispatch(updateTask(this.state.task));
+  }, 1000);
+
+  onTaskNameChange(e) {
+    let newName = e.target.value;
+
+    if (!newName) {
+      this.setState({
+        errors: {
+          ...this.state.errors,
+          name: 'Name is required',
+        },
+      });
+    }
+    else {
+      this.setState({
+        errors: {
+          ...this.state.errors,
+          name: null,
+        },
+      });
+    }
+
+    this.setState({
+      task: {
+        ...this.state.task,
+        name: newName,
+      },
+    });
+
+    if (newName) {
+      this.updateTaskDebounced();
+    }
   }
 
   onTaskDelete(e) {
     e.preventDefault();
 
-    this.setState({ submitting: true });
+    this.setState({submitting: true});
     this.props
       .dispatch(deleteTask(this.props.task))
-      .then(() => this.setState({
-        submitting: false,
-      }));
+      .then(() => this.setState({submitting: false}));
   }
 
   onTaskStateChange(e) {
@@ -62,18 +113,17 @@ class Task extends Component {
 
     this.setState({
       submitting: true,
-      checked: newState === 'complete',
+      task: {
+        ...this.state.task,
+        state: newState,
+      },
     });
-
     this.props
       .dispatch(updateTask({
         id: taskId,
         state: newState,
       }))
-      .then(() => this.setState({
-        submitting: false,
-        checked: null,
-      }));
+      .then(() => this.setState({submitting: false}));
   }
 }
 
