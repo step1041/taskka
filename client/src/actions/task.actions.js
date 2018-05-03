94850396eca483a158a398223c77af733435d5be
Stephen A. Wilson <Stephen-356@hotmail.com>
2018-05-03 12:19:50 -0400

    reformat task.actions.js

--- client/src/actions/task.actions.js
+++ client/src/actions/task.actions.js
@@ -2,30 +2,30 @@ import TaskkaApiClient from '../lib/taskka-api-client';
 import errorHandler from '../lib/error-handler';
 
 export const setTasks = (tasks) => ({
-  type: "TASKS.SET",
-  data: { tasks }
+  type: 'TASKS.SET',
+  data: {tasks},
 });
 
 export const addTask = (task) => ((dispatch) => {
   return TaskkaApiClient
-    .post('/tasks', { task })
+    .post('/tasks', {task})
     .then(({task: newTask}) => {
       dispatch({
         type: 'TASKS.ADD',
-        data: { task: newTask }
-      })
+        data: {task: newTask},
+      });
     })
     .catch(errorHandler);
 });
 
 export const updateTask = (task) => ((dispatch) => {
   return TaskkaApiClient
-    .patch(`/tasks/${task.id}`, { task })
+    .patch(`/tasks/${task.id}`, {task})
     .then(({task: newTask}) => {
       dispatch({
         type: 'TASKS.UPDATE',
-        data: { task: newTask }
-      })
+        data: {task: newTask},
+      });
     })
     .catch(errorHandler);
 });
