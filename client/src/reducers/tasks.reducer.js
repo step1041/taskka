363e50375c246d975feb42bedb7dff5a424c3e22
Stephen A. Wilson <Stephen-356@hotmail.com>
2018-05-12 16:49:02 -0400

    fix tasks state

--- client/src/reducers/tasks.reducer.js
+++ client/src/reducers/tasks.reducer.js
@@ -11,7 +11,7 @@ const tasksReducer = (state = initState, action) => {
       return initState;
 
     case ACTION_TYPES.TASKS.SET:
-      return List(action.data.TASKS);
+      return List(action.data.tasks);
 
     case ACTION_TYPES.TASKS.ADD:
       return state.push(action.data.task);
@@ -23,6 +23,9 @@ const tasksReducer = (state = initState, action) => {
     case ACTION_TYPES.TASKS.DELETE:
       index = state.findIndex((task) => task.id === action.data.task.id);
       return state.remove(index);
+
+    case ACTION_TYPES.PROJECTS.SET_CURRENT:
+      return List(action.data.project.tasks);
 
     default:
       return state;
