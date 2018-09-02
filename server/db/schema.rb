ac1680644f025bd218d1481bdfce198def25ab64
Stephen A. Wilson <Stephen-356@hotmail.com>
2018-09-02 14:50:52 -0400

    add completed_at to tasks

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_07_224910) do
+ActiveRecord::Schema.define(version: 2018_09_02_182415) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2018_05_07_224910) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "project_id"
+    t.datetime "completed_at"
     t.index ["state"], name: "index_tasks_on_state"
   end
 
