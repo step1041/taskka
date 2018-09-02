58cfa14e25868cb0725e6b954a6af1b86cb559e1
Stephen A. Wilson <Stephen-356@hotmail.com>
2018-09-02 17:50:32 -0400

    add Factories for server side tests

--- server/spec/rails_helper.rb
+++ server/spec/rails_helper.rb
@@ -6,6 +6,7 @@ ENV['RAILS_ENV'] ||= 'test'
 
 require File.expand_path('../../config/environment', __FILE__)
 
+Dir[Rails.root.join('spec/factories/**/*.rb')].each { |f| require f }
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 Dir[Rails.root.join('spec/data/**/*.rb')].each { |f| require f }
 
