26d368485ffc0d4cbff4c0b5735c1d0b81d11a87
Stephen A. Wilson <Stephen-356@hotmail.com>
2018-09-02 22:20:37 -0400

    add user newWorkingDay action

--- client/src/actions/user.actions.js
+++ client/src/actions/user.actions.js
@@ -1,3 +1,7 @@
+import moment from 'moment';
+
+import TaskkaApiClient from '../lib/taskka-api-client';
+
 import ACTION_TYPES from './action-types';
 import {setCurrentProject} from './project.actions';
 
@@ -7,7 +11,22 @@ export const setUser = (user) => ((dispatch) => {
       data: { user },
   });
 
+  if (moment(user.current_working_day).isBefore(moment(), 'day')) {
+    dispatch(newWorkingDay(user));
+  }
+
   return dispatch(setCurrentProject(user.default_project_id));
+});
+
+export const updateUser = (user) => ((dispatch) => {
+  TaskkaApiClient
+    .updateCurrentUser(user)
+    .then((savedUser) => {
+      dispatch({
+        type: ACTION_TYPES.USER.SET,
+        data: {user: savedUser},
+      });
+    });
 });
 
 export const login = (user) => ((dispatch) => {
@@ -21,4 +40,11 @@ export const login = (user) => ((dispatch) => {
 
 export const logout = () => ({
   type: ACTION_TYPES.USER.LOGOUT,
+});
+
+export const newWorkingDay = (user) => ((dispatch) => {
+  return dispatch(updateUser({
+    last_working_day: user.current_working_day,
+    current_working_day: moment(),
+  }));
 });
