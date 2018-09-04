1ac3d2c2152b833a379a23a157ea0199daeed40f
Stephen A. Wilson <Stephen-356@hotmail.com>
2018-09-04 11:06:39 -0400

    auto create a StateChange when task state changes

--- server/app/models/task.rb
+++ server/app/models/task.rb
@@ -1,5 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :project
+  has_many :state_changes, :dependent => :destroy
 
   validates :name, presence: true
   validates :state, presence: true
@@ -7,6 +8,7 @@ class Task < ApplicationRecord
   before_save -> do
     if self.state_changed?
       self.completed_at = self.state == "complete" ? Time.now : nil
+      self.state_changes.create!(:old_state => self.state_was, :new_state => self.state)
     end
   end
 end
